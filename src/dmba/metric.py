c9bb3368a9deedf59b45c5c69f607d5be22669ae
Peter Gedeck <peter.gedeck@gmail.com>
2021-02-16 18:45:28 -0500

    Closes #5

--- src/dmba/metric.py
+++ src/dmba/metric.py
@@ -20,10 +20,11 @@ def adjusted_r2_score(y_true, y_pred, model):
     n = len(y_pred)
     p = len(model.coef_)
     if p >= n - 1:
-      return 0
+        return 0
     r2 = r2_score(y_true, y_pred)
-    return 1 - (1 - r2) * (n - 1) /(n - p - 1)
-    
+    return 1 - (1 - r2) * (n - 1) / (n - p - 1)
+
+
 def AIC_score(y_true, y_pred, model=None, df=None):
     """ calculate Akaike Information Criterion (AIC) 
     Input:
@@ -31,16 +32,16 @@ def AIC_score(y_true, y_pred, model=None, df=None):
         y_pred: predicted values
         model (optional): predictive model
         df (optional): degrees of freedom of model
-        
+
     One of model or df is requried
     """
     if df is None and model is None:
-      raise ValueError('You need to provide either model or df')
+        raise ValueError('You need to provide either model or df')
     n = len(y_pred)
     p = len(model.coef_) + 1 if df is None else df
     resid = np.array(y_true) - np.array(y_pred)
-    sse = np.sum(resid ** 2) 
-    constant =  n + n * np.log(2*np.pi)
+    sse = np.sum(resid ** 2)
+    constant = n + n * np.log(2 * np.pi)
     return n * math.log(sse / n) + constant + 2 * (p + 1)
 
 
@@ -55,18 +56,18 @@ def BIC_score(y_true, y_pred, model=None, df=None):
     aic = AIC_score(y_true, y_pred, model=model, df=df)
     p = len(model.coef_) + 1 if df is None else df
     n = len(y_pred)
-    return aic - 2 * (p + 1) + math.log(n) * (p + 1)  
+    return aic - 2 * (p + 1) + math.log(n) * (p + 1)
 
 
 def regressionSummary(y_true, y_pred):
     """ print regression performance metrics 
-    
+
     Input:
         y_true: actual values
         y_pred: predicted values
     """
-    y_true = np.asarray(y_true)
-    y_pred = np.asarray(y_pred)
+    y_true = _toArray(y_true)
+    y_pred = _toArray(y_pred)
     y_res = y_true - y_pred
     metrics = [
         ('Mean Error (ME)', sum(y_res) / len(y_res)),
@@ -74,19 +75,26 @@ def regressionSummary(y_true, y_pred):
         ('Mean Absolute Error (MAE)', sum(abs(y_res)) / len(y_res)),
     ]
     if all(yt != 0 for yt in y_true):
-      metrics.extend([
-        ('Mean Percentage Error (MPE)', 100 * sum(y_res / y_true) / len(y_res)),
-        ('Mean Absolute Percentage Error (MAPE)', 100 * sum(abs(y_res / y_true) / len(y_res))),
-      ])
+        metrics.extend([
+            ('Mean Percentage Error (MPE)', 100 * sum(y_res / y_true) / len(y_res)),
+            ('Mean Absolute Percentage Error (MAPE)', 100 * sum(abs(y_res / y_true) / len(y_res))),
+        ])
     fmt1 = '{{:>{}}} : {{:.4f}}'.format(max(len(m[0]) for m in metrics))
     print('\nRegression statistics\n')
     for metric, value in metrics:
         print(fmt1.format(metric, value))
 
 
+def _toArray(y):
+    y = np.asarray(y)
+    if len(y.shape) == 2 and y.shape[1] == 1:
+        y = y.ravel()
+    return y
+
+
 def classificationSummary(y_true, y_pred, class_names=None):
     """ Print a summary of classification performance
-    
+
     Input:
         y_true: actual values
         y_pred: predicted values
@@ -96,34 +104,34 @@ def classificationSummary(y_true, y_pred, class_names=None):
     accuracy = accuracy_score(y_true, y_pred)
 
     print('Confusion Matrix (Accuracy {:.4f})\n'.format(accuracy))
-    
+
     # Pretty-print confusion matrix
     cm = confusionMatrix
 
     labels = class_names
     if labels is None:
         labels = [str(i) for i in range(len(cm))]
-   
+
     # Convert the confusion matrix and labels to strings
     cm = [[str(i) for i in row] for row in cm]
     labels = [str(i) for i in labels]
 
-    # Determine the width for the first label column and the individual cells    
+    # Determine the width for the first label column and the individual cells
     prediction = 'Prediction'
     actual = 'Actual'
     labelWidth = max(len(s) for s in labels)
     cmWidth = max(max(len(s) for row in cm for s in row), labelWidth) + 1
     labelWidth = max(labelWidth, len(actual))
-    
+
     # Construct the format statements
     fmt1 = '{{:>{}}}'.format(labelWidth)
     fmt2 = '{{:>{}}}'.format(cmWidth) * len(labels)
 
-    # And print the confusion matrix    
+    # And print the confusion matrix
     print(fmt1.format(' ') + ' ' + prediction)
     print(fmt1.format(actual), end='')
     print(fmt2.format(*labels))
-    
+
     for cls, row in zip(labels, cm):
         print(fmt1.format(cls), end='')
         print(fmt2.format(*row))
