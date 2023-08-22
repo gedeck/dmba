c94efe5803508062ac914513d10aa4d604b251b5
Rin Arakaki <rnarkkx@gmail.com>
2023-08-22 17:43:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
-'''
+"""
 Utility functions for "Data Mining for Business Analytics: Concepts, Techniques, and
 Applications in Python"
 
 (c) 2019-2023 Galit Shmueli, Peter C. Bruce, Peter Gedeck
-'''
-from pathlib import Path
+"""
 
 import setuptools
 
-
-def getVersion():
-    f = Path(__file__).parent / 'src' / 'dmba' / 'version.py'
-    lines = f.read_text().split('\n')
-    version = [s for s in lines if '__version__' in s][0]
-    version = version.split('=')[1].strip().strip("'")
-    return version
 
 setuptools.setup(
     install_requires=[
