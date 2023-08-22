8489fa53d7898a98cf6b1874f51fb10624da185b
Rin Arakaki <rnarkkx@gmail.com>
2023-08-22 17:38:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,24 +16,7 @@ def getVersion():
     version = version.split('=')[1].strip().strip("'")
     return version
 
-
-long_description = Path("README.md").read_text()
-
 setuptools.setup(
-    name="dmba",
-    version=getVersion(),
-    author="Peter Gedeck",
-    author_email="mail@petergedeck.com",
-    description=("Utility functions for 'Data Mining for Business Analytics: "
-                 "Concepts, Techniques, and Applications in Python'"),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/gedeck/dmba",
-    packages=setuptools.find_packages("src"),
-    package_dir={'': 'src'},
-    package_data={
-        "dmba": ["csvFiles/*.csv.gz", "csvFiles/*.zip"],
-    },
     install_requires=[
         'graphviz',
         'matplotlib',
@@ -45,16 +28,6 @@ setuptools.setup(
     extras_require={
         "ipython": ["ipython"],
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-
     test_suite='nose.collector',
     tests_require=['nose'],
 )
