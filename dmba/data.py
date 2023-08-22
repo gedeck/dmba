7e6418eb1ca7100569f74a45b241a4f42ef7455f
root <rnarkkx@gmail.com>
2023-08-22 10:51:43 +0000

    Update

--- dmba/data.py
+++ dmba/data.py
@@ -8,18 +8,19 @@ Applications in Python"
 from pathlib import Path
 from typing import Any, Union
 
-import pandas as pd
+import polars as pl
+from polars import DataFrame, Series
 
 
 DATA_DIR = Path(__file__).parent.parent / 'data'
 
 
-def load_data(name: str, **kwargs: Any) -> Union[pd.DataFrame, pd.Series]:
+def load_data(name: str, **kwargs: Any) -> Union[DataFrame, Series]:
     """ Returns the data either as a Pandas data frame or series """
     data_file = get_data_file(name)
     if not data_file.exists():
         raise ValueError('Data file {name} not found')
-    data = pd.read_csv(data_file, **kwargs)
+    data = pl.read_csv(data_file, **kwargs)
     if data.shape[1] == 1:
         return data[data.columns[0]]  # pylint: disable=E1136
     return data
